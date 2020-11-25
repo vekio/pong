6ef4e1ac163ebd8752d2fcf818f2019b647aa7ca
vekio <alberto@casta.me>
2020-11-25 18:16:53 +0100

    Add Menu class

---
+++ pong/menu/menu.py
@@ -0,0 +1,12 @@
+class VistaMenu(object):
+    """
+    Vista padre para todos los menús
+    """
+    pass
+
+class MainMenu(VistaMenu):
+    """
+    Vista menú principal
+    """
+    def __init__(self):
+        super().__init__()
